12bdefccbccdb1f4323bc800c6ee3d78f95cd745
cr-jkb <81223644+cr-jkb@users.noreply.github.com>
2023-11-28 16:34:51 +0100

    dynamic file path

--- Aufgabe4/src/main/java/aufgabe4/TimingComparison.java
+++ Aufgabe4/src/main/java/aufgabe4/TimingComparison.java
@@ -1,17 +1,23 @@
 package aufgabe4;
 
+import java.io.File;
 import java.io.IOException;
 
 public final class TimingComparison {
-    private TimingComparison() { }
+    private TimingComparison() {
+    }
 
     private static String projectDir = System.getProperty("user.dir");
     private static final int MIN_LENGTH = 20;
-    private static String[] testTextArray = {
-            projectDir + "\\src\\main\\resources\\testdatei.txt"
-    };
 
     public static void main(String[] args) throws IOException {
+        String filePath = projectDir + "\\Aufgabe4\\src\\main\\resources\\testdatei.txt";
+        if (!new File(filePath).exists()) {
+            filePath = projectDir + "\\src\\main\\resources\\testdatei.txt";
+        }
+        String[] testTextArray = {
+                filePath
+        };
         Procedural.main(testTextArray);
         Functional.main(testTextArray);
     }
